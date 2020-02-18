c2e161602671a5033301dcbb4bbeae1692bf0a92
ADINATA\adinata <adijunior1991@gmail.com>
2020-02-18 14:49:12 +0700

    Update Readme Dan Add Script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-"# redirectWA" 
+Script ini merupakan script untuk melakukan redirect ke Whatsapp namun sebelum di redirect event di pixel di tangkap dahulu agar di catat di facebook pixel sebagai trigger Add TO Chat
+
+Silahkan DI Modifikasi Sesuai Kebutuhan Anda
+
+
+Dapatkan Tips Programming Di Blog Saya : 
+
+www.adinata.co
+
+Salam,
+
+Adinata
---
+++ index.php
@@ -0,0 +1,37 @@
+<!-- Facebook Pixel Code Pixel Mawarsuperlaundry -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', 'xxx'); //xxx ganti dengan ID PIXEL ANDA
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1110127035838307&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+<script>
+fbq('track', 'AddToCartWHatsaapp', {
+value: 5000,
+currency: 'IDR'
+});
+</script>
+
+
+<script>
+
+  setTimeout(function(){
+    window.location = "https://api.whatsapp.com/send?phone=62822689xxxx&text=Halo%20mawarfresh%20saya%20ingin%20daftar%20Agen" //xxx ganti dengan nomor WA Anda
+  }, 3000);
+
+
+</script>
+<h1> Sedang Mengarahkan Ke Customer Service Kami <br>
+    Mohon Tunggu Sebentar Ya...!!!
+  </h1>
